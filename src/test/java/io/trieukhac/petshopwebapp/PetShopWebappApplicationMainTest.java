c4b1d7877629d193a66c3c2978e1c7c782e85362
TrieuKhac <caotrieu333@gmail.com>
2026-03-07 15:47:23 +0700

    test again

--- src/test/java/io/trieukhac/petshopwebapp/PetShopWebappApplicationMainTest.java
+++ src/test/java/io/trieukhac/petshopwebapp/PetShopWebappApplicationMainTest.java
@@ -10,6 +10,7 @@ class PetShopWebappApplicationMainTest {
 
   @Test
   void runApp_startsAndClosesContext() {
+
     // truyền args rỗng; runApp đã set WebApplicationType.NONE nên không khởi web server
     try (ConfigurableApplicationContext ctx = PetShopWebappApplication.runApp(new String[] {})) {
       assertNotNull(ctx); // đảm báo context được tạo
